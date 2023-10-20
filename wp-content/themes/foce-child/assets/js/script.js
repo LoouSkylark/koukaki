c331d51c91368d666c12f98991b493993e17f4da
Elodie Louette <elodie.louette@orange.fr>
2023-10-20 13:17:47 +0200

    Toutes dernières mofifs

--- wp-content/themes/foce-child/assets/js/script.js
+++
@@ -1,9 +0,0 @@
-// Run after document has loaded...
-jQuery(document).ready(function($){
-    $(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    if (scroll >= 500) { // If user scrolls down 500px...
-      $('h2').fadeIn(5000);
-    }
-    });
-  });
