f75e86e36c7838dc98fc38409c895fe65b732d45
Elodie Louette <elodie.louette@orange.fr>
2023-11-14 12:26:37 +0100

    Site terminé

--- wp-content/themes/foce-child/js/script.js
+++ wp-content/themes/foce-child/js/script.js
@@ -1,39 +1,37 @@
 console.log ('start')
 
-// Run after document has loaded...
+// Exécuter après le chargement du document...
 jQuery(document).ready(function($){
 
-// Paralaxe du logo
+// Parallaxe du logo
 $(window).scroll(function() {
   var offset = $(window).scrollTop();
   $('.logo-container').css('transform', 'translateY(' + offset * 0.5 + 'px)');
 });
 
-// Animation au scroll des titres
-
-   const observer = new IntersectionObserver(entries => {
-    // Loop over the entries
-    entries.forEach(entry => {
-      // si l'élément est visible
-      if (entry.isIntersecting) {
-        // ajouter classe 
-        entry.target.classList.add("animPart1");
-      }
-        else {
-          // supprimer  classe 
-        entry.target.classList.remove("animPart1");
-        }
-    });
-  });
+// Animation des titres de section au scroll
+const observer = new IntersectionObserver(entries => {
+// Boucle pour chaque entrée
+entries.forEach(entry => {
+  // si l'élément est visible
+  if (entry.isIntersecting) {
+    // ajouter la classe 
+    entry.target.classList.add("animPart1");
+  }
+    else {
+    // supprimer la classe 
+    entry.target.classList.remove("animPart1");
+    }
+});
+});
  
-    // Première partie du titre
+  // Première partie du titre
   const sections = document.querySelectorAll(".title-part1")
     sections.forEach(section => {
     observer.observe(section);
   });  
 
   const observer2 = new IntersectionObserver(entries => {
- 
     entries.forEach(entry => {
       if (entry.isIntersecting) {
         entry.target.classList.add("animPart2");
@@ -44,7 +42,7 @@ $(window).scroll(function() {
     });
   });
 
-  // Deuxieme partie du titre
+  // Deuxième partie du titre
   const sections2 = document.querySelectorAll(".title-part2")
     sections2.forEach(section => {
     observer2.observe(section);  
@@ -54,7 +52,7 @@ $(window).scroll(function() {
 // Animation des  nuages
 $(window).scroll(function() {
   var scroll = $(window).scrollTop();
-  if (scroll >= 1000) { // If user scrolls down 500px...
+  if (scroll >= 1000) { // Si l'utilisateur fait défiler 500px vers le bas
 
     $( ".cloud1" ).animate({
       right: "60%",
@@ -66,97 +64,45 @@ $(window).scroll(function() {
   });
 
 // Slider personnages
-  var swiper = new Swiper(".mySwiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 70,
-      modifier: 1,
-      slideShadows: false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-  });
-console.log(Swiper)
-  }); 
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 70,
+    modifier: 1,
+    slideShadows: false,
+  },
+});
+}); 
 
 
 // Menu burger
 
+// Sélection des éléments du DOM
 const button = document.querySelector('.button-menu');
 const nav = document.querySelector('.menuburger');
 const backdrop = document.querySelector('.backdrop');
 const remove = document.querySelector('.site-main');
 const remove2 = document.querySelector('footer');
+//Ajout d'un écouteur d'événements pour le bouton du menu
 button.addEventListener('click', () => {
   nav.classList.toggle('open');
   remove.classList.toggle('remove');
   remove2.classList.toggle('remove');
 
 });
-
+// Ajout d'un écouteur d'événements pour l'élément backdrop
 backdrop.addEventListener('click', () => {
   nav.classList.remove('open');
   remove.classList.remove('remove');
   remove2.classList.remove('remove');
 });
 
-// button burger 
+// Ajout d'un deuxième écouteur d'événements pour le bouton du menu
 button.addEventListener('click', ()=> {
   button.classList.toggle('active');
 });
-
-/*
-// Animation menu 
-const animemenu = document.querySelectorAll('.animation');
-
-button.addEventListener('click', () => {
-  animemenu.forEach(function(element) {
-    element.classList.toggle('animationbas');
-  });
-}); */
-
-  /*  $(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    if (scroll >= 500) { // If user scrolls down 500px...
-      $('h2').fadeIn(5000);
-    }
-    });
-
-// Apparition des titres
-jQuery(document).ready(function($){
-  $(window).scroll(function() {
-  var scroll = $(window).scrollTop();
-  if (scroll >= 500) { // If user scrolls down 500px...
-    $('h2').css({'animation':'500px'});
-  }
-  });
-});
-
-    $('.site-footer::after, .site-footer ul::after, .site-footer::before, .story__article::after, #studio h2::before, #studio h2::after, .fleur').addClass('myclass');
-
-    $(window).scroll(function() {
-      var scroll = $(window).scrollTop();
-
-        $('.site-footer').css('backgroung-color', 'green');
-        console.log ($('.site-footer'))
-    }); 
-
-  });
-
-  // Run after document has loaded...
-jQuery(document).ready(function($){
-  $(window).scroll(function() {
-  var scroll = $(window).scrollTop();
-  if (scroll >= 500) { // If user scrolls down 500px...
-    $('h2').animate({'height':'500px'});
-  }
-  });
-});
-*/
-
--- wp-content/themes/foce-child/menu.php
+++ wp-content/themes/foce-child/menu.php
@@ -4,14 +4,13 @@
         <img class="logoburger" src="wp-content\themes\foce-child\assets\logoburger.png" alt="">
 
         <ul class="new-ul animation">
-            <li><a href="#story">Histoire</a></li>
-            <li><a href="#characters">Personnages</a></li>
-            <li><a href="#place">Lieu</a></li>
-            <li><a href="#studio">Studio Koukaki</a></li>
+            <li><a href="#story"><span class="title-part2">Histoire</span></a></li>
+            <li><a href="#characters"><span class="title-part2">Personnages</span></a></li>
+            <li><a href="#place"><span class="title-part2">Lieu</span></a></li>
+            <li><a href="#studio"><span class="title-part1">Studio</span><span class="title-part2">&nbsp;Koukaki</span></a></li>
             <p>STUDIO KOUKAKI</p>
         </ul>
-
-
+       		
         <div class="flowersburger">
 
             <img class="orchidburger" src="wp-content\themes\foce-child\assets\orchid.png" alt="">
